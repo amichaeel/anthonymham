5de52cc4bfccf797c53fa3bdc38bfa496ebaa1f0
amichaeel <mail@anthonymham.com>
2024-01-14 19:26:03 -0500

    fixed build errors

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react"
-import { Link, Button, Element, Events, animateScroll as scroll, scrollSpy } from 'react-scroll';
+import { Link, Button, Element, Events, animateScroll as scroll, scrollSpy } from "react-scroll"
 import Image from "next/image"
 
 export default function Home() {
@@ -27,11 +27,11 @@ export default function Home() {
       <main className="px-4">
         <section className="pb-16">
           <h1 className="font-semibold text-4xl mb-4 text-white">
-            <span className="animate-text bg-gradient-to-r from-teal-500 via-blue-500 to-purple-500 bg-clip-text text-transparent">Hello, I'm Anthony.</span>
+            <span className="animate-text bg-gradient-to-r from-teal-500 via-blue-500 to-purple-500 bg-clip-text text-transparent">Hello, I&apos;m Anthony.</span>
             <br />
             <span className=" text-3xl text-white/60">A software engineer / computer science student from Miami, Fl.</span>
           </h1>
-          <p className="text-white/70 text-lg">I'm Anthony, a current student at Florida International University (FIU) studying computer science. Currently, I work on the team at FIU's Applied Research Center as a software developer, primarily focusing on customer-facing solutions. </p>
+          <p className="text-white/70 text-lg">I&apos;m Anthony, a current student at Florida International University (FIU) studying computer science. Currently, I work on the team at FIU&apos;s Applied Research Center as a software developer, primarily focusing on customer-facing solutions. </p>
         </section>
 
         {/* Projects */}
@@ -40,7 +40,7 @@ export default function Home() {
           <div className="space-y-4">
 
             <div className="flex gap-4 items-center bg-slate-800 rounded-xl p-5 cursor-pointer hover:bg-slate-700">
-              <Image className="rounded-lg" src="/placeholder.png" width={40} height={40} />
+              <Image alt="placeholder-logo" className="rounded-lg" src="/placeholder.png" width={40} height={40} />
               <span>
                 <h3 className="text-white font-medium text-base">Orbit</h3>
                 <h3 className="text-white/70 text-sm">A decentralized social media platform.</h3>
@@ -48,7 +48,7 @@ export default function Home() {
             </div>
 
             <div className="flex gap-4 items-center bg-slate-800 rounded-xl p-5 cursor-pointer hover:bg-slate-700">
-              <Image className="rounded-lg" src="/placeholder.png" width={40} height={40} />
+              <Image alt="placeholder-logo" className="rounded-lg" src="/placeholder.png" width={40} height={40} />
               <span>
                 <h3 className="text-white font-medium text-base">Promptora</h3>
                 <h3 className="text-white/70 text-sm">A place to share and discover AI prompts.</h3>
@@ -56,10 +56,10 @@ export default function Home() {
             </div>
 
             <div className="flex gap-4 items-center bg-slate-800 rounded-xl p-5 cursor-pointer hover:bg-slate-700">
-              <Image className="rounded-lg" src="/placeholder.png" width={40} height={40} />
+              <Image alt="placeholder-logo" className="rounded-lg" src="/placeholder.png" width={40} height={40} />
               <span>
                 <h3 className="text-white font-medium text-base">Journey</h3>
-                <h3 className="text-white/70 text-sm">A web app making it easy to create iteneries when you're on vacation.</h3>
+                <h3 className="text-white/70 text-sm">A web app making it easy to create iteneries when you&apos;re on vacation.</h3>
               </span>
             </div>
 
