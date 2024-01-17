1ac3e4ee36ae8d8db37bdf8bf3d0363afd146a8e
amichaeel <mail@anthonymham.com>
2024-01-16 23:46:45 -0500

    responsive behavior improvement

--- components/ExperienceModal.js
+++ components/ExperienceModal.js
@@ -1,13 +1,13 @@
 import React from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faClock } from "@fortawesome/free-solid-svg-icons"
+import { faArrowRight, faClock } from "@fortawesome/free-solid-svg-icons"
 
-export default function ExperienceModal({location, position, description, timeWorked, technologies}) {
+export default function ExperienceModal({location, position, description, timeWorked, technologies, link}) {
   return (
-    <div className="grid grid-cols-2 gap-4 transition-all group/experience duration-300 rounded-xl cursor-default lg:group-hover:opacity-40 lg:group-hover:hover:opacity-100 lg:hover:bg-slate-600/10 py-4">
+    <div className="grid md:grid-cols-2 gap-4 transition-all group/experience duration-300 rounded-xl lg:cursor-pointer cursor-default lg:group-hover:opacity-40 lg:group-hover:hover:opacity-100 lg:hover:bg-slate-600/10 py-4">
       <span className="text-white/30 text-sm uppercase font-semibold lg:group-hover/experience:translate-x-3 transition-all duration-300">{timeWorked} <FontAwesomeIcon icon={faClock} className="ml-2 opacity-0 lg:group-hover/experience:opacity-100 transition-all duration-300" /></span>
       <div className="flex flex-col">
-        <span className="text-white lg:group-hover/experience:text-teal-200 ">{location}</span>
+        <span className="text-white lg:group-hover/experience:text-teal-300 ">{location} <FontAwesomeIcon className="text-xs ml-1 lg:group-hover/experience:-rotate-45 transition-transform duration-300" icon={faArrowRight} /></span>
         <span className="text-white/40 text-sm">{position}</span>
         <p className="mt-2 text-white/60">{description}</p>
 
